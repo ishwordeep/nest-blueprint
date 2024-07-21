053e078b1afe17386ce2e4602d1d0bdb624ce59f
ishwordeep <ishwor055@gmail.com>
2024-07-21 17:40:04 +0545

    some cmodule created

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,14 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PackagesModule } from './app/modules/packages/packages.module';
+import { ServicesModule } from './app/modules/services/services.module';
+import { SubscriptionsModule } from './app/modules/subscriptions/subscriptions.module';
 
 @Module({
   imports: [DatabaseModule, ConfigModule.forRoot({
     isGlobal: true,
-  }), AuthModule, UserModule],
+  }), AuthModule, UserModule, PackagesModule, ServicesModule, SubscriptionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
